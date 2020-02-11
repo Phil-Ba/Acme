e2cb1cf187810d3d3d3ecc43ea0bc5ac11286fa1
phil-ba <phi.ba@yandex.com>
2020-02-11 21:28:52 +0100

    Added some main classes

---
+++ acme-categories/src/main/kotlin/at/bayava/acme/categories/main/CategoriesApplication.kt
@@ -0,0 +1,12 @@
+package at.bayava.acme.categories.main
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CategoriesApplication {
+
+}
+
+fun main() {
+
+}
---
+++ acme-product-inventory/src/main/kotlin/at/bayava/acme/productinventory/main/ProductInventoryApplication.kt
@@ -0,0 +1,12 @@
+package at.bayava.acme.productinventory.main
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ProductInventoryApplication {
+
+}
+
+fun main() {
+
+}
