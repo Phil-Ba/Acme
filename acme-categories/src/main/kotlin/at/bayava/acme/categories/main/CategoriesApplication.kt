01e190defcd1ae6ed99cacc22f66f7eee486ed93
phil-ba <phi.ba@yandex.com>
2020-02-13 15:25:45 +0100

    Added scan for base packages

--- acme-categories/src/main/kotlin/at/bayava/acme/categories/main/CategoriesApplication.kt
+++ acme-categories/src/main/kotlin/at/bayava/acme/categories/main/CategoriesApplication.kt
@@ -3,10 +3,8 @@ package at.bayava.acme.categories.main
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@SpringBootApplication
-class CategoriesApplication {
-
-}
+@SpringBootApplication(scanBasePackages = ["at.bayava.acme"])
+class CategoriesApplication
 
 fun main() {
     SpringApplication.run(CategoriesApplication::class.java)
--- acme-product-inventory/src/main/kotlin/at/bayava/acme/productinventory/main/ProductInventoryApplication.kt
+++ acme-product-inventory/src/main/kotlin/at/bayava/acme/productinventory/main/ProductInventoryApplication.kt
@@ -3,10 +3,8 @@ package at.bayava.acme.productinventory.main
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-@SpringBootApplication
-class ProductInventoryApplication {
-
-}
+@SpringBootApplication(scanBasePackages = ["at.bayava.acme"])
+class ProductInventoryApplication
 
 fun main() {
     SpringApplication.run(ProductInventoryApplication::class.java)
