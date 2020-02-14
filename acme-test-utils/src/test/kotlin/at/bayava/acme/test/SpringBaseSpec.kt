91cb6576d176b1cd3f388531fd6cfc3c9782f4d0
phil-ba <phi.ba@yandex.com>
2020-02-14 21:01:17 +0100

    Moved activeProfiles to right place and added test debugging

--- acme-test-utils/src/test/kotlin/at/bayava/acme/test/SpringBaseSpec.kt
+++ acme-test-utils/src/test/kotlin/at/bayava/acme/test/SpringBaseSpec.kt
@@ -5,12 +5,14 @@ import io.kotlintest.extensions.TestListener
 import io.kotlintest.specs.AnnotationSpec
 import io.kotlintest.spring.SpringListener
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.MOCK,
     classes = [SpringBaseSpec::class, SpringTestConfig::class]
 )
+@ActiveProfiles("test")
 class SpringBaseSpec : AnnotationSpec() {
 
     override fun isolationMode(): IsolationMode = IsolationMode.InstancePerTest
--- acme-test-utils/src/test/kotlin/at/bayava/acme/test/SpringTestConfig.kt
+++ acme-test-utils/src/test/kotlin/at/bayava/acme/test/SpringTestConfig.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import org.testcontainers.containers.GenericContainer
 import javax.sql.DataSource
@@ -14,7 +13,6 @@ import javax.sql.DataSource
 @TestConfiguration
 @EnableTransactionManagement
 @ComponentScan("at.bayava.acme")
-@ActiveProfiles("test")
 class SpringTestConfig {
 
     @Bean(destroyMethod = "stop")
