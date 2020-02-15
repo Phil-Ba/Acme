016eab0d6e6afc45234537dc6458b40ec113a810
phil-ba <phi.ba@yandex.com>
2020-02-15 22:52:24 +0100

    Config classes for AOP and caching

---
+++ acme-product-inventory/src/main/kotlin/at/bayava/acme/productinventory/config/AopConfig.kt
@@ -0,0 +1,8 @@
+package at.bayava.acme.productinventory.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured
+
+@Configuration
+@EnableSpringConfigured
+class AopConfig
---
+++ acme-product-inventory/src/main/kotlin/at/bayava/acme/productinventory/config/CachingConfig.kt
@@ -0,0 +1,8 @@
+package at.bayava.acme.productinventory.config
+
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableCaching
+class CachingConfig
