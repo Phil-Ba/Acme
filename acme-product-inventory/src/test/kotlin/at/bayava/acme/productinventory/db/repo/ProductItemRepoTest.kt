5b8a116f9b67dee921b2a8f600cb7f9b2db29aac
phil-ba <phi.ba@yandex.com>
2020-02-16 10:16:40 +0100

    Fix for test

--- acme-product-inventory/src/test/kotlin/at/bayava/acme/productinventory/db/repo/ProductItemRepoTest.kt
+++ acme-product-inventory/src/test/kotlin/at/bayava/acme/productinventory/db/repo/ProductItemRepoTest.kt
@@ -1,16 +1,35 @@
 package at.bayava.acme.productinventory.db.repo
 
+import at.bayava.acme.categories.service.CategoryService
+import at.bayava.acme.productinventory.client.model.Category
 import at.bayava.acme.productinventory.db.model.ProductItem
 import at.bayava.acme.productinventory.db.model.ProductType
 import at.bayava.acme.test.SpringBaseSpec
 import io.kotlintest.matchers.collections.shouldBeEmpty
 import io.kotlintest.matchers.collections.shouldContainExactly
+import io.mockk.every
+import io.mockk.mockkClass
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
 
 @Transactional
+@ContextConfiguration(classes = [ProductItemRepoTest.Config::class])
 class ProductItemRepoTest : SpringBaseSpec() {
+
+    open class Config {
+        @Bean
+        @Primary
+        open fun categoryServiceMock(): CategoryService {
+            val categoryService = mockkClass(CategoryService::class)
+            every { categoryService.fetchCategories() } returns listOf(Category(1, "testProductType", 1.0, 2.0, 3.0))
+            return categoryService
+        }
+    }
+
     @Autowired
     lateinit var productRepo: ProductRepo
 
@@ -23,15 +42,17 @@ class ProductItemRepoTest : SpringBaseSpec() {
 
     @Test
     fun afterInsertEntryShouldBeInDb() {
-        val product = productRepo.save(
-            ProductItem(
-                -1,
-                ProductType(-1, "testProductType", emptyList()),
-                LocalDate.of(1990, 12, 1),
-                123.45
-            )
+        val productType = ProductType(-1, "testName", "testProductType", emptyList())
+        val productItem = ProductItem(
+            -1,
+            productType,
+            LocalDate.of(1990, 12, 1),
+            123.45
         )
+
+        val product = productRepo.save(productItem)
         productRepo.flush()
+
         val result = productRepo.findAll()
 
         result.shouldContainExactly(product)
