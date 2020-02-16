1201e286b0f31b052a3a381741eec804c163dfe5
phil-ba <phi.ba@yandex.com>
2020-02-16 22:47:36 +0100

    Final fixes

--- acme-product-inventory/src/main/docker/entrypoint.sh
+++ acme-product-inventory/src/main/docker/entrypoint.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+./wait-for-it.sh categories:8080
 exec /app/app.jar
+
--- acme-ui/src/main/docker/entrypoint.sh
+++ acme-ui/src/main/docker/entrypoint.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+./wait-for-it.sh categories:8080
+./wait-for-it.sh product-inventory:8080
 exec /app/app.jar
